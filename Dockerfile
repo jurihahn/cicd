d09b31426bf59b3f446599a8e952bba9336ac32d
Juri Hahn <juri@hahn21.de>
2020-07-02 14:06:42 +0200

    add update.php to download CI/CD files

--- Dockerfile
+++ Dockerfile
@@ -6,3 +6,5 @@ RUN apt-get update -y && \
 apt-get upgrade -y && \
 apt-get install -y zlib1g-dev php php-zip php-xml && \
 npm install -g node-minify
+
+COPY update.php /update.php
---
+++ update.php
@@ -0,0 +1,55 @@
+<?php
+
+function download($file_source, $file_target, $headers)
+{
+	$opts = array(
+		'http'=>array(
+			'method'=>"GET",
+			'header'=>$headers
+		)
+	);
+	$context = stream_context_create($opts);
+
+	$rh = fopen($file_source, 'rb', null, $context);
+	$wh = fopen($file_target, 'wb');
+	if($rh === false || $wh === false)
+	{
+		// error reading or opening file
+		return false;
+	}
+	while(!feof($rh))
+	{
+		if(fwrite($wh, fread($rh, 1024)) === FALSE)
+		{
+			// 'Download error: Cannot write to file ('.$file_target.')';
+			return false;
+		}
+	}
+	fclose($rh);
+	fclose($wh);
+	// No error
+	return TRUE;
+}
+
+$dir = __DIR__;
+
+if(isset($argv))
+{
+	parse_str(implode('&', array_slice($argv, 1)), $_GET);
+}
+$prj_id = $_GET['cicd_project_id'];
+$gitlaburl = $_GET['gitlaburl'];
+$private_token = $_GET['private_token'];
+$files = json_decode($_GET['files']);
+
+echo ' gitlaburl=' . $gitlaburl . "\n";
+
+foreach ($files as $file)
+{
+	echo 'download "' . $file . '", with private key "' . $private_token . '"' . "\n";
+	download(
+		$gitlaburl . 'api/v4/projects/' . $prj_id . '/repository/files/' . urlencode($file) . '/raw?ref=master',
+		$dir . '/' . $file,
+		"PRIVATE-TOKEN: " . $private_token . "\r\n");
+	echo ' - download "' . $file . '" done!' . "\n";
+}
